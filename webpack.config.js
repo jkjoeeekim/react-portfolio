01315461f351749cd981382e7cfc6963986a1af1
Joseph Kim <jkjoeeekim@live.com>
2021-10-26 06:03:43 -0700

    Update gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const htmlWebpackPlugin = new HtmlWebpackPlugin({
+//   template: path.join(__dirname, "frontend/index.html"),
+//   filename: "./index.html"
+// });
 
 module.exports = {
   entry: './frontend/react_portfolio_redux.jsx', // specify entry to todo_redux.jsx
   output: {
     path: path.resolve(__dirname), // resolves path into an absolute
-    filename: 'bundle.js'
+    filename: './bundle.js'
   },
   module: {
     rules: [
@@ -14,12 +19,20 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/env', '@babel/react']
-        }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
   devtool: 'source-map', // creates a bundle.js.map - without this chrome would only tell you where your errors are in the bundle file
+  // plugins: [htmlWebpackPlugin],
   resolve: {
     extensions: [".js", ".jsx", "*"] // import a file - import something from 'something'
-  }
+  },
+  // devServer: {
+  //   port: 5500
+  // }
 };
